9f7b7b26400fc8154f76daff5dd1f3272222b274
khushi_23 <khushiagrawal230705@gmail.com>
2025-08-21 18:22:04 +0530

    day 7 programs

--- day7/db_connect.py
+++ day7/db_connect.py
@@ -1,9 +1,10 @@
 import pymysql
 
-connection = pymysql.connect(host='localhost', user='root', password='ShyamAgra@230', database='khushi_db',
+connection = pymysql.Connect(host='localhost', user='root', password='ShyamAgra@230', database='khushi_db',
  port=3306, charset='utf8mb4' )
 
 if connection:
     print('database connected')
 else:
     print('Database connection failed')
+
---
+++ gsss_sic/day 2/error.py
@@ -0,0 +1,14 @@
+#Name error : when we are asling python to do something which it cannot do
+n = -0b1111
+b
+
+#index error: we wre asking to print something which it cannot print 
+
+#value error: when u are trying to cast some value into other and it is not possible
+num = int(sys.argv[0])
+num
+
+#type error: range expect atleast 1 argument
+
+
+#index error: list index out of range
